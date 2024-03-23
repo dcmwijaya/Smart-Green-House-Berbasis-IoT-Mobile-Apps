fca8892f39d51eac32c4c29e53df34527acb86ac
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-23 11:27:24 +0700

    Update Final_Project_Edspert_Batch10_ESP32Project.ino

--- Src/Arduino IDE Project/Device_ESP32/Final_Project_Edspert_Batch10_ESP32Project.ino
+++ Src/Arduino IDE Project/Device_ESP32/Final_Project_Edspert_Batch10_ESP32Project.ino
@@ -70,35 +70,23 @@ void bacaSensor() {
 
 // Kendali Otomatis Pompa
 void Threshold(){
-  // Jika suhu udara rendah, kelembaban tinggi, dan intensitas cahaya rendah -> dapat diasumsikan bahwa kondisinya sedang malam hari, maka :
-  if (temp >= 0 && temp < 16) {
-    if (hum > 90 && hum <=100) {
-      if (ldr >= 500) {  
-        pump = "OFF"; // status pompa: OFF
-        digitalWrite(PIN_WATERPUMP, relayOFF); // Pompa air mati
-      }
-    }
-  }
-  
-  // Jika suhu udara sedang, kelembaban sedang, dan intensitas cahaya sedang -> dapat diasumsikan bahwa kondisinya sedang pagi hari, maka :
-  if (temp >= 16 && temp <= 34) {
-    if (hum >= 30 && hum <= 90) {
-      if (ldr >= 200 && ldr < 500) {  
-        pump = "OFF"; // status pompa: OFF
-        digitalWrite(PIN_WATERPUMP, relayOFF); // Pompa air mati
-      }
-    }
-  }
-  
-  // Jika suhu udara tinggi, kelembaban rendah, dan intensitas cahaya tinggi -> dapat diasumsikan bahwa kondisinya sedang siang hari, maka :
-  if (temp > 34 && temp <= 100) {
-    if (hum >= 0 && hum < 30) {
-      if (ldr < 200) {
-        pump = "ON"; // status pompa: ON
-        digitalWrite(PIN_WATERPUMP, relayON); // Pompa air menyala
-      }
-    }
-  } 
+  // Jika suhu udara rendah, kelembaban tinggi, dan intensitas cahaya rendah, maka :
+  if (temp >= 0 && temp < 16) { if (hum > 90 && hum <=100) { if (ldr >= 500) {
+    pump = "OFF"; // status pompa: OFF
+    digitalWrite(PIN_WATERPUMP, relayOFF); // Pompa air mati
+  } } }
+
+  // Jika suhu udara sedang, kelembaban sedang, dan intensitas cahaya sedang, maka :
+  if (temp >= 16 && temp <= 34) { if (hum >= 30 && hum <= 90) { if (ldr >= 200 && ldr < 500) {
+    pump = "OFF"; // status pompa: OFF
+    digitalWrite(PIN_WATERPUMP, relayOFF); // Pompa air mati
+  } } }
+
+  // Jika suhu udara tinggi, kelembaban rendah, dan intensitas cahaya tinggi, maka :
+  if (temp > 34 && temp <= 100) { if (hum >= 0 && hum < 30) { if (ldr < 200) {
+    pump = "ON"; // status pompa: ON
+    digitalWrite(PIN_WATERPUMP, relayON); // Pompa air menyala
+  } } } 
 
   // Jika kondisi tanah basah maka :
   if (moisture >= wetSoil){
